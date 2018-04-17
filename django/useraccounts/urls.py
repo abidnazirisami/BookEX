05aeefe306ff295c3c82297fe76a361f69d8d600
RaidaAsh <raidanahian@gmail.com>
2018-04-18 01:45:33 +0600

    Version 2.1.7

--- django/useraccounts/urls.py
+++ django/useraccounts/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('profile/', views.showUserProfile, name='profile'),
-    path('<username>/', views.showProfile, name='viewprofile'),
+    path('user/<username>/', views.showProfile, name='viewprofile'),
     path('edit/', views.editUserProfile, name='editprofile')
 ]
