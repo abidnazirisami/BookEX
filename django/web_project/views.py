5c63166786fd3d9685c32b2e7f96506eb0dcb482
abidnazirisami <abidnazirisami@gmail.com>
2018-04-17 22:39:02 +0600

    Version 2.3.8

--- django/web_project/views.py
+++ django/web_project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import *
 from pages.models import *
+from request.models import *
 from django.http import HttpResponseRedirect
 import isbnlib
 from django.contrib.auth.decorators import login_required
@@ -14,4 +15,25 @@ def homepage(request):
 			current_user.first_name = current_user.username
 			current_user.save()
 			newUser = OurUser.objects.create(user = current_user,user_name = current_user.username)
+		userid = OurUser.objects.get(user = current_user)
+		donated_list =[]
+		if Boiii.objects.filter(id_id = userid).exists():
+			donated_list = Boiii.objects.filter(id_id = userid)
+		#print(donated_list)
+		requested_list = []
+		user_list = []
+		for donate_worthy in donated_list:
+			cur_isbn = donate_worthy.isbn.isbn
+			#print(cur_isbn)
+			if Wishlist.objects.filter(isbn = cur_isbn):
+				wished_object = list(Wishlist.objects.filter(isbn = cur_isbn))
+				#print(wished_object)
+				for wish in wished_object:
+					requested_list.append(wish)
+					cur_user = list(OurUser.objects.filter(user_id = wish.user_id))
+					user_list.append(cur_user[0])
+					#print(cur_user[0])
+					#print(wish)
+		#print(requested_list)
+		return render(request,'home.html',context={'request_list':zip(requested_list,user_list)})
 	return render(request, 'home.html')
