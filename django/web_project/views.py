f0a440d68f27e7b7efe1aa7a2120c9c6e47ef28a
RaidaAsh <raidanahian@gmail.com>
2018-04-21 23:07:35 +0600

    Version 3.1.1

--- django/web_project/views.py
+++ django/web_project/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from fuzzywuzzy import fuzz
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from typing import List, Any
+
 
 def homepage(request):
 	current_user = request.user
@@ -19,20 +21,22 @@ def homepage(request):
 		userid = OurUser.objects.get(user = current_user)
 		donated_list =[]
 		if Boiii.objects.filter(id_id = userid).exists():
-			donated_list = Boiii.objects.filter(id_id = userid)
-		#print(donated_list)
+			donated_list = Boiii.objects.values('id_id','isbn_id').filter(id_id = userid).distinct()
+		print(donated_list)
 		requested_list = []
 		user_list = []
 		profile_list=[]
 		notification_count=0
+		book_count = []
 		for donate_worthy in donated_list:
-			cur_isbn = donate_worthy.isbn.isbn
+			cur_isbn = donate_worthy['isbn_id']
 			#print(cur_isbn)
 			if Wishlist.objects.filter(isbn = cur_isbn):
 				wished_object = list(Wishlist.objects.filter(isbn = cur_isbn))
 				#print(wished_object)
 				for wish in wished_object:
 					requested_list.append(wish)
+					book_count.append( Boiii.objects.values('id_id','isbn_id').filter(id_id = userid,isbn_id = cur_isbn).count())
 					cur_user = list(OurUser.objects.filter(user_id = wish.user_id))
 					user_list.append(cur_user[0])
 					profile_list.append(User.objects.get(username=cur_user[0].user_name))
@@ -47,7 +51,7 @@ def homepage(request):
 			print(request.POST['username'])
 			print(request.POST['isbn'])
 			################################################################
-		return render(request,'home.html',context={'request_list':zip(requested_list,user_list, profile_list),'notification_count':notification_count})
+		return render(request,'home.html',context={'request_list':zip(requested_list,user_list, profile_list,book_count),'notification_count':notification_count})
 	return render(request, 'home.html')
 
 def about(request):
